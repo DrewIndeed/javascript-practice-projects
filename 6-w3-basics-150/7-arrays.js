cfe3d4358313fd6e6d79dfa9d0a38a2901201e9b
Andrew Le <s3820098@rmit.edu.vn>
2021-09-11 16:06:44 +0700

    max sum of consecutive items in srray

--- 6-w3-basics-150/7-arrays.js
+++ 6-w3-basics-150/7-arrays.js
@@ -4,11 +4,35 @@ function swapItemsAt(array, firstIndex, secondIndex) {
     return array;
 }
 
-console.log(swapItemsAt([1, 2, 3, 4, 5], 0, 2));
+// console.log(swapItemsAt([1, 2, 3, 4, 5], 0, 2));
 
+
+// kth greatest element in array
 function kthGreatestOf(k, array) {
     array.sort((a, b) => b - a);
     return array[k - 1];
 }
 
-console.log(kthGreatestOf(2, [8, 2, 3, 4, 9, 6, 7, 1]));
+// console.log(kthGreatestOf(2, [8, 2, 3, 4, 9, 6, 7, 1]));
+
+
+// get the greatest sum when sum a consecutive number of digit in the array
+function maxSumEveryConsecutiveDigit(array, consecutiveNum) {
+    if (consecutiveNum > array.length || consecutiveNum < 1) return "Nah ah!";
+
+    let max = 0;
+    let runTo = array.length - consecutiveNum + 1;
+    for (let i = 0; i < runTo; i++) {
+        let sum = 0;
+        for (let j = i; j < (i + consecutiveNum); j++) {
+            console.log(array[j])
+            sum = sum + array[j];
+        }
+        console.log("sum:", sum);
+        console.log("------")
+        if (sum > max) max = sum;
+    }
+    return max;
+}
+
+console.log("Max is:", maxSumEveryConsecutiveDigit([8, 2, 3, 4, 9, 6, 7, 1], 4));
