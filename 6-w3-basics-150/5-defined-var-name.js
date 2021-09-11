feac96e870ada7610641c50cb6c9e502024f5131
Andrew Le <s3820098@rmit.edu.vn>
2021-09-11 10:57:52 +0700

    5-defined-var

---
+++ 6-w3-basics-150/5-defined-var-name.js
@@ -0,0 +1,12 @@
+this['Andrew'] = 22
+this['Phiphi'] = 21
+this['Sophia'] = 28
+this['Nikola'] = 100
+this[0] = 'Sunday'
+this[1] = 'Monday'
+console.log(this['Andrew']);
+console.log(this['Phiphi']);
+console.log(this['Sophia']);
+console.log(this['Nikola']);
+console.log(this[0]);
+console.log(this[1]);
