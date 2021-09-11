fa2e910a653484fb8097f2211de7df9f285f785f
Andrew Le <s3820098@rmit.edu.vn>
2021-09-11 15:18:41 +0700

    kth greatest of array using sort

--- 6-w3-basics-150/7-arrays.js
+++ 6-w3-basics-150/7-arrays.js
@@ -5,3 +5,10 @@ function swapItemsAt(array, firstIndex, secondIndex) {
 }
 
 console.log(swapItemsAt([1, 2, 3, 4, 5], 0, 2));
+
+function kthGreatestOf(k, array) {
+    array.sort((a, b) => b - a);
+    return array[k - 1];
+}
+
+console.log(kthGreatestOf(2, [8, 2, 3, 4, 9, 6, 7, 1]));
