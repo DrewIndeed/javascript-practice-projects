9bdadf053b39bfa3870ca59336ca395372850a7a
Andrew Le <s3820098@rmit.edu.vn>
2021-09-11 22:51:28 +0700

    8. count until there is only one digit left

---
+++ 6-w3-basics-150/8-to-one-digit-count.js
@@ -0,0 +1,31 @@
+/**
+ * Write a JavaScript program to find the number of
+ * times to replace a given number with the sum of its digits
+ * until it convert to a single digit number.
+ */
+
+
+function countUntilOneDigit(number) {
+  // sum digit as a separated function
+  function sumDigits(number) {
+    let sum = 0;
+    while (number > 0) {
+      let digit = Math.floor(number % 10);
+      number = number / 10;
+      sum = sum + digit;
+    }
+    return sum;
+  }
+
+  if (number < 0) number = -number;
+  if (number <= 9) return number;
+  else {
+    let sumDigit = sumDigits(number);
+    while (sumDigit > 9) {
+      sumDigit = sumDigits(sumDigit);
+    }
+    return sumDigit;
+  }
+}
+
+console.log(countUntilOneDigit(12121212));
