24996e141a5b82bec61989db4466932fa3a3c8b9
Andrew Le <s3820098@rmit.edu.vn>
2021-09-15 13:49:11 +0700

    Update 3. to digit array

--- 7-w3-es6-part1/3-to-digits.js
+++ 7-w3-es6-part1/3-to-digits.js
@@ -2,15 +2,6 @@
  * Write a JavaScript program to converts a specified number to an array of digits.
 */
 
-// test number
-const testNum = 20102002;
-
-// test printing
-console.log([...`${testNum}`]);
-console.log(...`${testNum}`);
-const toArray = [...`${testNum}`];
-
-// try to using map to act on each item
-let toDigitArray = toArray.map(digit => parseInt(digit));
-console.log(toDigitArray);
+const toDigitArray = (num) => [...`${num}`].map(strDigit => parseInt(strDigit));
+console.log(toDigitArray(12345678));
 
