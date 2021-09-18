7de06cebcc74bd50eb7f028d2f24671b2d91747b
Andrew Le <s3820098@rmit.edu.vn>
2021-09-18 20:59:12 +0700

    test randomizing

--- 7-w3-es6-part1/7-random-hex-color/7-random-hex-color.js
+++ 7-w3-es6-part1/7-random-hex-color/7-random-hex-color.js
@@ -1,2 +1,3 @@
 console.log(document.getElementsByTagName('body')[0].style);
 document.getElementsByTagName('body')[0].style.backgroundColor = "#ff1416";
+console.log(Math.floor(Math.random() * 10));
