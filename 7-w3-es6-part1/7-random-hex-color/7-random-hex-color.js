9e8103ac20265aa4abb7c1d9343f055b8f46872c
Andrew Le <s3820098@rmit.edu.vn>
2021-09-18 21:43:33 +0700

    adjust condition + add header

--- 7-w3-es6-part1/7-random-hex-color/7-random-hex-color.js
+++ 7-w3-es6-part1/7-random-hex-color/7-random-hex-color.js
@@ -1,3 +1,17 @@
-console.log(document.getElementsByTagName('body')[0].style);
-document.getElementsByTagName('body')[0].style.backgroundColor = "#ff1416";
-console.log(Math.floor(Math.random() * 10));
+/**
+ * Write a JavaScript program to generate a random hexadecimal color code.
+*/
+
+// console.log(document.getElementsByTagName('body')[0].style);
+// console.log(Math.floor(Math.random() * 10));
+
+function createHex() {
+    const hex = Math.floor(Math.random() * 255).toString(16);
+    return (hex.length == 1) ? "0" + hex : hex;
+}
+
+setInterval(() => {
+    let randomColor = "#" + createHex() + createHex() + createHex();
+    console.log(randomColor);
+    document.getElementsByTagName('body')[0].style.backgroundColor = randomColor;
+}, 100);
