755286b548f6dcceeae753d8c21bd3b778876494
Andrew Le <s3820098@rmit.edu.vn>
2021-09-21 13:03:18 +0700

    13, all possible pairs: add 2nd way

--- 7-w3-es6-part1/13-possible-pairs.js
+++ 7-w3-es6-part1/13-possible-pairs.js
@@ -1,7 +1,7 @@
 /**
- * Write a JavaScript program to create a new array out of 
+ * Write a JavaScript program to create a new array out of
  * the two supplied by creating each possible pair from the arrays
-*/
+ */
 
 // Andrew way
 function allPossiblePairs(array1, array2) {
@@ -12,4 +12,14 @@ function allPossiblePairs(array1, array2) {
   return rs;
 }
 console.log(allPossiblePairs([1, 2], [3, 4]));
+console.log(allPossiblePairs([1, 2, 3], [4, 5, 6, 7]));
 
+// ES6
+const allPossiblePairs2 = (array1, array2) =>
+  array1.reduce(
+    (container, array1Item) =>
+      container.concat(array2.map((array2Item) => [array1Item, array2Item])),
+    []
+  );
+console.log(allPossiblePairs2([1, 2], [3, 4]));
+console.log(allPossiblePairs2([1, 2, 3], [4, 5, 6, 7]));
