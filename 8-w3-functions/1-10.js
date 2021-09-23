41b5f593ac036fbad138bc0a71d29507fb5d5ced
Andrew Le <s3820098@rmit.edu.vn>
2021-09-23 19:36:48 +0700

    Add all combination for string char

--- 8-w3-functions/1-10.js
+++ 8-w3-functions/1-10.js
@@ -1,7 +1,22 @@
-const reverseString = (str) => Array.from(str).reverse().join('');
-console.log(reverseString("Andrew"));
+const reverseString = (str) => Array.from(str).reverse().join("");
+// console.log(reverseString("Andrew"));
 
-const isPalindrome = (str) => reverseString(str).toLowerCase().replace(/[^a-z]/gi, '') === str.toLowerCase().replace(/[^a-z]/gi, '');
-console.log(isPalindrome("madam"));
-console.log(isPalindrome("Andrew Le Nguyen"));
-console.log(isPalindrome("A dog! A panic in a pagoda."));
+const isPalindrome = (str) =>
+  reverseString(str)
+    .toLowerCase()
+    .replace(/[^a-z]/gi, "") === str.toLowerCase().replace(/[^a-z]/gi, "");
+// console.log(isPalindrome("madam"));
+// console.log(isPalindrome("Andrew Le Nguyen"));
+// console.log(isPalindrome("A dog! A panic in a pagoda."));
+// console.log(isPalindrome("A new order began, a more Roman age bred Rowena."));
+
+const combiStringChar = (str) => {
+  let resultArray = [[]];
+  for (const item1 of Array.from(str)) {
+    let tempArray = [];
+    for (const item2 of resultArray) tempArray.push(item2.concat([item1]));
+    resultArray = [...resultArray, ...tempArray];
+  }
+  return resultArray.slice(1).map((e) => e.join(""));
+};
+console.log(combiStringChar("dog"));
