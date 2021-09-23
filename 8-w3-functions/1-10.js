1b162a351e48875ebbd75615aeccbdd060443a3f
Andrew Le <s3820098@rmit.edu.vn>
2021-09-23 19:06:32 +0700

    Add isPalindrome

--- 8-w3-functions/1-10.js
+++ 8-w3-functions/1-10.js
@@ -1,3 +1,7 @@
 const reverseString = (str) => Array.from(str).reverse().join('');
 console.log(reverseString("Andrew"));
 
+const isPalindrome = (str) => reverseString(str).toLowerCase().replace(/[^a-z]/gi, '') === str.toLowerCase().replace(/[^a-z]/gi, '');
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("Andrew Le Nguyen"));
+console.log(isPalindrome("A dog! A panic in a pagoda."));
