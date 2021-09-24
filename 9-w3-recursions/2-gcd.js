08e881082d2ccd28eeef5bd6ffdb7cc7968da2c5
Andrew Le <s3820098@rmit.edu.vn>
2021-09-24 20:06:38 +0700

    Create 2-gcd.js and add GCD iteration + add testings

---
+++ 9-w3-recursions/2-gcd.js
@@ -0,0 +1,41 @@
+// Euclid
+function gcdIterate1(a, b) {
+  a = Math.abs(a);
+  b = Math.abs(b);
+  if (a == b || b == 0) return a;
+  if (a == 0) return b;
+  while (a != b) {
+    if (a > b) a = a - b;
+    else b = b - a;
+  }
+  return a;
+}
+
+// Euclidean
+function gcdIterate2(a, b) {
+  a = Math.abs(a);
+  b = Math.abs(b);
+  if (a == b || b == 0) return a;
+  if (a == 0) return b;
+  while (true) {
+    if (b == 0) return a;
+    a %= b;
+    if (a == 0) return b;
+    b %= a;
+  }
+}
+
+console.log("Using iterate 1:");
+console.log(gcdIterate1(2154, 0));
+console.log(gcdIterate1(0, 2154));
+console.log(gcdIterate1(2154, 428));
+console.log(gcdIterate1(428, 2154));
+console.log(gcdIterate1(0, 0));
+console.log(gcdIterate1(22, 22));
+console.log("Using iterate 2:");
+console.log(gcdIterate2(2154, 0));
+console.log(gcdIterate2(0, 2154));
+console.log(gcdIterate2(2154, 428));
+console.log(gcdIterate2(428, 2154));
+console.log(gcdIterate2(0, 0));
+console.log(gcdIterate2(22, 22));
