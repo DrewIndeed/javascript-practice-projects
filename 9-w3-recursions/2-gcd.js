0a1935ed5885888f91b1a56559f7ff5a7c9cbaa9
Andrew Le <s3820098@rmit.edu.vn>
2021-09-24 22:34:13 +0700

    Adjust gcdInterate2 + add 2 versions of recusions

--- 9-w3-recursions/2-gcd.js
+++ 9-w3-recursions/2-gcd.js
@@ -15,8 +15,6 @@ function gcdIterate1(a, b) {
 function gcdIterate2(a, b) {
   a = Math.abs(a);
   b = Math.abs(b);
-  if (a == b || b == 0) return a;
-  if (a == 0) return b;
   while (true) {
     if (b == 0) return a;
     a %= b;
@@ -28,14 +26,45 @@ function gcdIterate2(a, b) {
 console.log("Using iterate 1:");
 console.log(gcdIterate1(2154, 0));
 console.log(gcdIterate1(0, 2154));
-console.log(gcdIterate1(2154, 428));
+console.log(gcdIterate1(-2154, 428));
 console.log(gcdIterate1(428, 2154));
 console.log(gcdIterate1(0, 0));
 console.log(gcdIterate1(22, 22));
 console.log("Using iterate 2:");
 console.log(gcdIterate2(2154, 0));
 console.log(gcdIterate2(0, 2154));
-console.log(gcdIterate2(2154, 428));
+console.log(gcdIterate2(-2154, 428));
 console.log(gcdIterate2(428, 2154));
 console.log(gcdIterate2(0, 0));
 console.log(gcdIterate2(22, 22));
+
+// Euclid
+function gcdRecursive1(a, b) {
+  if (a == b) return a;
+  if (a == 0) return b;
+  if (b == 0) return a;
+  a = Math.abs(a);
+  b = Math.abs(b);
+  return a > b ? gcdRecursive1(a - b, b) : gcdRecursive1(a, b - a);
+}
+
+// Euclidean
+function gcdRecursive2(a, b) {
+  if (b == 0) return a;
+  return gcdRecursive2(b, a % b);
+}
+
+console.log("Using recursive 1:");
+console.log(gcdRecursive1(2154, 0));
+console.log(gcdRecursive1(0, 2154));
+console.log(gcdRecursive1(-2154, 428));
+console.log(gcdRecursive1(428, 2154));
+console.log(gcdRecursive1(0, 0));
+console.log(gcdRecursive1(22, 22));
+console.log("Using recursive 2:");
+console.log(gcdRecursive2(2154, 0));
+console.log(gcdRecursive2(0, 2154));
+console.log(gcdRecursive2(-2154, 428));
+console.log(gcdRecursive2(428, 2154));
+console.log(gcdRecursive2(0, 0));
+console.log(gcdRecursive2(22, 22));
