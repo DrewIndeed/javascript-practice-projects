ba3d43a531e68d08e27e4588bdbc77995f40ae75
Andrew Le <s3820098@rmit.edu.vn>
2021-09-24 15:06:55 +0700

    Add bubble sort

--- 8-w3-functions/21-29.js
+++ 8-w3-functions/21-29.js
@@ -10,7 +10,6 @@ const allCombiFromArrays = (arr, len) => {
   }
   return result.filter((a) => a.length == len);
 };
-
 // console.log(allCombiFromArrays([1, 2, 3], 2));
 
 // exclude 22
@@ -19,4 +18,18 @@ const firstUniqueChar = (str) =>
   Array.from(new Set(Array.from(str))).filter(
     (c) => Array.from(str).indexOf(c) === Array.from(str).lastIndexOf(c)
   )[0];
-console.log(firstUniqueChar("abacdffpffzzzzdbc"));
+// console.log(firstUniqueChar("abacdffpffzzzzdbc"));
+
+const bubbleSort = (arr) => {
+  for (let i = 0; i <= arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+};
+console.log(bubbleSort([9, 2, 6, 1, 8, 33, 25, 146, 5, 4, 1, 2, 1000, 8]));
