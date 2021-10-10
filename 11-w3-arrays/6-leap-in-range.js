b63b4dbefdf1586c1983899d77565b1fae1c944a
Andrew Le <s3820098@rmit.edu.vn>
2021-10-10 20:18:47 +0700

    Add finding leap year in a range of years

---
+++ 11-w3-arrays/6-leap-in-range.js
@@ -0,0 +1,12 @@
+const isLeapYear = (y) => (y % 4 === 0 && y % 100 !== 0) || y % 400 === 0;
+const range = (from, to) => {
+  if (to - from === 1) return [from];
+  else {
+    let rs = range(from, to - 1);
+    rs.push(to - 1);
+    return rs;
+  }
+};
+const leapYearFromTo = (y1, y2) =>
+  range(y1, y2 + 1).filter((y) => isLeapYear(y));
+console.log(leapYearFromTo(2000, 2012));
