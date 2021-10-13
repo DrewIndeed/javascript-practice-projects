2925fb677a06a1b67a31bbb639380379ea08ded4
Andrew Le <s3820098@rmit.edu.vn>
2021-10-13 16:17:07 +0700

    Add methods of srings

---
+++ 12-w3-strings/0-methods.js
@@ -0,0 +1,23 @@
+/**
+ * 1/ charAt(index) -> get a character from a given index number
+ * 2/ charCodeAt() -> get Unicode of a character at a given index
+ * 3/ concat() -> concat another string to tail
+ * 4/ startsWith() -> check if a string starts with another string
+ * 5/ endsWith() -> check if a string ends with another string
+ * 6/ fromCharCode() -> Convert Unicode to a character
+ * 7/ includes() -> check if includes a string
+ * 8/ indexOf() -> get index of the first character found
+ * 9/ lastIndexOf() -> get index of the last character found
+ * 10/ match(regex) -> searches a string for a match against a regular expression, and returns the matches, as an Array object.
+ * 11/ replace(regex, replacement) -> replace certain part of a string with something
+ * 12/ search(regex) -> search for a given string and return index
+ * 13/ slice() -> extract a part of a string from given indexes
+ * 14/ split() -> split a string by a param
+ * 15/ substr(startIndex, numberOfChars) -> extract a number of char from a starting index
+ * 16/ substring(startIndex, endIndex) -> extract a number of char from starting and ending indexes
+ * 17/ toLowerCase() 
+ * 18/ toUpperCase()
+ * 19/ trim() -> remove whitespaces from both ends
+ * 20/ valueOf() -> return the primitive value of a string object
+ * 21/ repeat(numOfRepeat) -> repeats a string with a number of copies
+ */
