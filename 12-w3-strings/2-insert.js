89915ed8530bfa31b732260e75a8863657606f93
Andrew Le <s3820098@rmit.edu.vn>
2021-10-24 16:27:05 +0700

    Add insert string

--- 12-w3-strings/2-insert.js
+++ 12-w3-strings/2-insert.js
@@ -1,1 +1,11 @@
-const insert = (org, tar = "", index = 0) => {};
+const insert = (org, tar = "", index = 0) => {
+  if (tar === "") return org;
+  if (index === 0) return tar + org;
+  if (index === -1) return org + tar;
+  return org.slice(0, index) + tar + org.slice(index, org.length);
+};
+
+console.log(insert("Le Nguyen"));
+console.log(insert("Le Nguyen", "Andrew "));
+console.log(insert("Le Nguyen", "Andrew ", 3));
+console.log(insert("Le Nguyen", " Andrew", -1));
