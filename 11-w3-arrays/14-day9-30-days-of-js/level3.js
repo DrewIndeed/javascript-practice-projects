7b27e78702077a41e100df504673b21f39a4ef33
Andrew Le <s3820098@rmit.edu.vn>
2021-11-19 21:15:39 +0700

    Add comments to show last exercise

--- 11-w3-arrays/14-day9-30-days-of-js/level3.js
+++ 11-w3-arrays/14-day9-30-days-of-js/level3.js
@@ -67,3 +67,11 @@ console.log(
   "Most populated country:",
   mostOfCountries(countries, 5, "population", "population")
 );
+
+// ! IT IS JUST TOO LONG, BUT NOT DIFFICULT, SO I WONT DO IT
+// Try to develop a program which calculate measure of central tendency of a
+// sample(mean, median, mode) and measure of variability(range, variance,
+// standard deviation).In addition to those measures find the min, max, count,
+// percentile, and frequency distribution of the sample.You can create an object
+// called statistics and create all the functions which do statistical
+// calculations as method for the statistics object.Check the output below.
