45a158053d519b08d6d5cc5ccbb560c56a92ca81
Andrew Le <s3820098@rmit.edu.vn>
2021-11-19 14:18:04 +0700

    Add getting a certain number of most spoken languages

--- 11-w3-arrays/14-day9-30-days-of-js/level3.js
+++ 11-w3-arrays/14-day9-30-days-of-js/level3.js
@@ -1,16 +1,20 @@
 import { countries } from "./data.js";
 
 // Sort countries by capital, by population
-console.log("--------------------------- SORT COUNTRIES BY CAPITAL NAMES ---------------------------\n");
+console.log(
+  "--------------------------- SORT COUNTRIES BY CAPITAL NAMES ---------------------------\n"
+);
 countries
   .sort((c1, c2) => (c1.capital > c2.capital ? 1 : -1))
   .forEach((item, index) =>
     console.log(
       `${index + 1}/ '${item.capital}' is the capital of '${item.name}'\n`
     )
+  );
+
+console.log(
+  " --------------------------- SORT COUNTRIES BY POPULATION ---------------------------\n"
 );
-	
-console.log(" ---------------------------SORT COUNTRIES BY POPULATION ---------------------------\n");
 countries
   .sort((c1, c2) => (c1.population > c2.population ? 1 : -1))
   .forEach((item, index) =>
@@ -18,3 +22,30 @@ countries
       `${index + 1}/ '${item.population}' is the population of '${item.name}'\n`
     )
   );
+
+console.log(
+  " --------------------------- QUERY MOST SPOKEN LANGUAGES ---------------------------\n"
+);
+// Get a number of most spoken languages
+const mostSpokenLanguages = (data, quanity) => {
+  if (Object.keys(data).length < quantity || quantity < 0)
+    return "Invalid quantity!";
+
+  // create a counting object for countries' languages
+  const countResultObj = data.reduce((frequency, country) => {
+    country.languages.forEach((lg) => {
+      if (!frequency[lg]) frequency[lg] = 1;
+      else frequency[lg]++;
+    });
+    return frequency;
+  }, {});
+
+  // sort it from large to small
+  const sortByFrequency = Object.entries(countResultObj).sort((lg1, lg2) =>
+    lg1[1] > lg2[1] ? -1 : 1
+  );
+
+  // slicing according to the quantity and return the result
+  return sortByFrequency.slice(0, quanity);
+};
+console.log(mostSpokenLanguages(countries, 5));
