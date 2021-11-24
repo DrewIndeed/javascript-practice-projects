d319031d8e4c6f936dd37943cbee87121ae3dc53
Andrew Le <s3820098@rmit.edu.vn>
2021-11-24 20:48:29 +0700

    245. Done TODO Step 2

--- 18-vanilla-js-cities-type-ahead/script.js
+++ 18-vanilla-js-cities-type-ahead/script.js
@@ -10,7 +10,27 @@ fetch(endpoint)
   .then((response) => response.json())
   .then((data) => citiesResult.push(...data));
 
+// print out to see response
+console.log(citiesResult);
+
 // TODO Step 2: Write function to match with what needs to be seached
+const matchSearch = (queryStr, dataArray) => {
+  // filter out cite and state names that match query string
+  return dataArray.filter((location) => {
+    // create new regex with global and case-insensitive tag
+    const regex = new RegExp(queryStr, "gi");
+
+    // using match() and regex to compare with the names
+    return location.city.match(regex) || location.state.match(regex);
+  });
+};
+
+// ! using setTimeOut because without it, this will be printed 
+// ! asynchronously -> print before getting the response form the API
+setTimeout(() => {
+  console.log(matchSearch("bos", citiesResult));
+}, 3000);
+
 // TODO Step 3: Add onChange + onKeyUp event listener for search input
 // TODO Step 4: Write function to render fetched data as list items
 // TODO Step 4.1: Highlight search part from list items' names of cities and states
