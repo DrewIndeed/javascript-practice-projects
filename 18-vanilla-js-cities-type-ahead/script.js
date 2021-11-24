a26a6fba52a975e9d7bb25b52d5e442ad65e952e
Andrew Le <s3820098@rmit.edu.vn>
2021-11-24 19:50:19 +0700

    243. Create separate js file + add todos

---
+++ 18-vanilla-js-cities-type-ahead/script.js
@@ -0,0 +1,9 @@
+const endpoint =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+// TODO Step 1: Fetch data from API endpoint and store in array
+// TODO Step 2: Write function to match with what needs to be seached
+// TODO Step 3: Add onChange + onKeyUp event listener for search input
+// TODO Step 4: Write function to render fetched data as list items
+// TODO Step 4.1: Highlight search part from list items' names of cities and states
+// TODO Step 5: Write a function of put commas into population value
