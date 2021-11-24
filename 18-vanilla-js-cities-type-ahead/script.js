b813a501a33e0c070cb2d8fea21c8c62a12a9b3c
Andrew Le <s3820098@rmit.edu.vn>
2021-11-24 20:11:30 +0700

    244. Done TODO Step 1

--- 18-vanilla-js-cities-type-ahead/script.js
+++ 18-vanilla-js-cities-type-ahead/script.js
@@ -2,6 +2,14 @@ const endpoint =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 
 // TODO Step 1: Fetch data from API endpoint and store in array
+// container for result after fetching
+const citiesResult = [];
+
+// fetching from endpoint
+fetch(endpoint)
+  .then((response) => response.json())
+  .then((data) => citiesResult.push(...data));
+
 // TODO Step 2: Write function to match with what needs to be seached
 // TODO Step 3: Add onChange + onKeyUp event listener for search input
 // TODO Step 4: Write function to render fetched data as list items
