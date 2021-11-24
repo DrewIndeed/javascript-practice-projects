fea158129604ceb0ce9262ceb10e447ca8e5259d
Andrew Le <s3820098@rmit.edu.vn>
2021-11-24 21:23:54 +0700

    246. Done TODO Step 3 and 4

--- 18-vanilla-js-cities-type-ahead/script.js
+++ 18-vanilla-js-cities-type-ahead/script.js
@@ -25,13 +25,35 @@ const matchSearch = (queryStr, dataArray) => {
   });
 };
 
-// ! using setTimeOut because without it, this will be printed 
+// ! using setTimeOut because without it, this will be printed
 // ! asynchronously -> print before getting the response form the API
 setTimeout(() => {
   console.log(matchSearch("bos", citiesResult));
-}, 3000);
+}, 2000);
 
 // TODO Step 3: Add onChange + onKeyUp event listener for search input
 // TODO Step 4: Write function to render fetched data as list items
+// get ui component from DOM tree
+const searchInput = document.querySelector(".search");
+const suggestionsList = document.querySelector(".suggestions");
+
+const displayResult = (e) => {
+  const matchingPlaces = matchSearch(e.target.value, citiesResult);
+  const renderAsListItems = matchingPlaces.map(
+    (location) => `
+    <li>
+      <span class="name">${location.city}, ${location.state}</span>
+      <span class="population">${location.population}</span>
+    </li>
+  `
+  ).join("");
+  suggestionsList.innerHTML = renderAsListItems;
+};
+
+// add event listenners to handle input change
+searchInput.addEventListener("change", displayResult);
+searchInput.addEventListener("keyup", displayResult);
+searchInput.addEventListener("keydown", displayResult);
+
 // TODO Step 4.1: Highlight search part from list items' names of cities and states
 // TODO Step 5: Write a function of put commas into population value
